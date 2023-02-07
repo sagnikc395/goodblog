bfe4910286a35148e6eb8966e66c5e341ceb5fc1
sgnikc395 <sagnikchatterjee395@gmail.com>
2023-02-07 11:56:43 +0530

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# goodblog
+
+A app made for markNine for neog camp level 0 . 
+It lists some good blog recommendations out of the all amazing blogs and substacks present.
+
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>goodblog</h1>
+      <p>Checkout my favourite blogs.Select a genre to get started.</p>
+    </div>
+  )
+}
+
+export default App
