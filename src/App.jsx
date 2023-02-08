09f8d8e2caa5d6c7ae0b07d51432de528473267a
sagnikc395 <sagnikchatterjee395@gmail.com>
2023-02-09 03:33:54 +0530

    src: decoupled data and view

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,6 @@
 import { useState } from "react";
 import "./styles/index.css";
-
-const blogs = {
-  web: [
-    {
-      title: "Rust is the future of Javascript Infrastructure",
-      rating: "3.75",
-      href: "https://leerob.io/blog/rust/",
-    },
-    {
-      title: "React is Becoming a Black Box",
-      rating: "4.75",
-      href: "https://jaredpalmer.com/blog/react-is-becoming-a-black-box/",
-    },
-  ],
-  design: [
-    { title: "Economics Title #1", rating: "2.75", href: "#" },
-    { title: "Economics Title #2", rating: "4.5", href: "#" },
-  ],
-  history: [
-    { title: "History Title #1", rating: "3.75", href: "#" },
-    { title: "History Title #2", rating: "5", href: "#" },
-  ],
-};
+import blogs from "./blogs";
 
 const genreList = Object.keys(blogs);
 
